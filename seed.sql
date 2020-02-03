8f805bf8730502b1812754c27a0291de92dfb6b9
njohnbritto <nirmaljulietneena.johnbritto@charter.com>
2020-02-03 08:07:45 -0700

    my changes

---
+++ seed.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department
+(
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(30),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+	id int NOT NULL AUTO_INCREMENT,
+    title varchar(30),
+    salary decimal(8,2),
+    department_id int(10),
+	PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee
+(
+	id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(30),
+    last_name varchar(30),
+	role_id int(10),
+    manager_id int(10),
+	PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+insert into department (name) values ("IT");
+insert into department (name) values ("Accounts");
+insert into department (name) values ("Admin");
+
+insert into role (title,salary,department_id) values ("Manager",100000.00,1);
+insert into role (title,salary,department_id) values ("Developer",90000.00,1);
+insert into role (title,salary,department_id) values ("Analyst",70000.00,2);
+insert into role (title,salary,department_id) values ("Admin",80000.00,3);
+
+insert into employee (first_name, last_name, role_id, manager_id) values ("Neena","John",2,2);
+insert into employee (first_name, last_name, role_id, manager_id) values ("Kiran","Alam",2,2);
+insert into employee (first_name, last_name, role_id, manager_id) values ("Warren","Berra",1,null);
+insert into employee (first_name, last_name, role_id, manager_id) values ("Alan","Dizol",2,2);
+
+
+
+
+
